16e57e78e148cb23093d6fe93b7b05c5a90b43a4
Zach-Deladev <zach.delapenha.developer@gmail.com>
2022-07-26 18:26:45 +0000

    Add basic navbar from react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Search from "./pages/SearchCards";
 import CardDetail from "./pages/CardDetail";
-
+import NavBar from "./components/NavBar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Search />} />
         <Route path="/card-detail" element={<CardDetail />} />

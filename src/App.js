6b047a9e6d3b87fad3f33182c864be230df2f373
Zach-Deladev <zach.delapenha.developer@gmail.com>
2022-07-26 16:00:09 +0000

    Add brower router to app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element={} />
+        <Route path="" element={} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

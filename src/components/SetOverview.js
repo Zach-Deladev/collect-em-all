bd293ae4a3bf3af4226e2da6b3d26178c84c52fb
Zach-Deladev <zach.delapenha.developer@gmail.com>
2022-08-05 06:40:55 +0000

    Add images from API of set logos to SetOverview page

--- src/components/SetOverview.js
+++ src/components/SetOverview.js
@@ -1,7 +1,6 @@
-import { render } from "@testing-library/react";
 import React from "react";
-// import styles from "./SetOverview.module.css";
 import setOptions from "../data/sets";
+import styles from "./SetOverview.module.css"
 
 export default function SetOverview() {
   const sets = setOptions;
@@ -9,10 +8,16 @@ export default function SetOverview() {
   return (
     <div>
       {sets.map((set) => {
-        return <h2>{set.label}</h2>;
+        return (
+          <div className={styles.cardD} key={set.value}>
+            <div className={styles.card}>
+              <img src={set?.images?.logo} alt="" />
+              <h2>{set.label}</h2>;<p>Number of cards: {set.printedTotal}</p>
+              <p>Release date: {set.releaseDate}</p>
+            </div>
+          </div>
+        );
       })}
     </div>
   );
-
-  console.log(sets);
 }
